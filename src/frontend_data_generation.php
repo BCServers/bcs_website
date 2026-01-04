c2a01af9f28587dd8a101ab862597716a2fac14b
Andrei Smoliakov <main@avsm.me>
2026-01-04 18:38:33 +0200

    Refactor frontend_data_generation.php

--- src/frontend_data_generation.php
+++ src/frontend_data_generation.php
@@ -9,85 +9,70 @@ function load_json($file) {
 }
 
 function find_user($username, $USER_DB) {
-	return array_column($USER_DB, null,
-		'username')[$username] ?? null;
+	return array_column($USER_DB, null, 'username')[$username] ?? null;
 }
 
 function find_server($server_name, $SERVER_DB) {
-	return array_column($SERVER_DB, null,
-		'srv_name')[$server_name] ?? null;
+	return array_column($SERVER_DB, null, 'srv_name')[$server_name] ?? null;
 }
 
 function generate_awg_keys($user_awg_key_data, $SERVER_DB, $relay_name) {
 	$keys = array();
 
-	for($i = 0; $i < count($user_awg_key_data); ++$i) {
-		$this_srv_key_data = $user_awg_key_data[$i]->key_data;
-		$this_srv_name = $user_awg_key_data[$i]->srv_name;
-		$this_srv_data = find_server($this_srv_name, $SERVER_DB);
-		$this_relay_srv_data = make_relay($this_srv_data, 
-			find_server($relay_name, $SERVER_DB));
+	foreach($user_awg_key_data as $ukd) {
+		$srv = find_server($ukd->srv_name, $SERVER_DB);
+		$relay_srv = make_relay($srv, find_server($relay_name, $SERVER_DB));
 
-		usort($this_srv_key_data,
+		usort($ukd->key_data, // sort by IP
 			function ($a, $b) {
 				$last_digit = function($ip) {return ip2long($ip) % 256;};
 				return $last_digit($a->IP) - $last_digit($b->IP);
 			}
 		);
 
-		$this_srv_keys = array();
+		$is_srv_relay = ($srv->srv_name === $relay_name);
+		$keys[$ukd->srv_name] = array();
 
-		for($j = 0; $j < count($this_srv_key_data); ++$j) {
-			$this_srv_keys[$j]['main']['native'] = generate_awg_native_conf(
-				$this_srv_key_data[$j], $this_srv_data);
-			$this_srv_keys[$j]['relay']['native'] =
-				($this_srv_data->srv_name === $relay_name) ? '' :
-					generate_awg_native_conf(
-						$this_srv_key_data[$j], $this_relay_srv_data);
+		foreach($ukd->key_data as $i => $key_data) {
+			$key['main']['native'] =
+				generate_awg_native_conf($key_data, $srv);
+			$key['relay']['native'] = $is_srv_relay ? '' :
+				generate_awg_native_conf($key_data, $relay_srv);
 
-			$this_srv_keys[$j]['main']['encoded'] = encode_config(
-				generate_awg_full_conf(
-					$this_srv_key_data[$j], $this_srv_data, $j+1));
-			$this_srv_keys[$j]['relay']['encoded'] =
-				($this_srv_data->srv_name === $relay_name) ? '' :
-					encode_config(generate_awg_full_conf(
-						$this_srv_key_data[$j], $this_relay_srv_data, $j+1));
+			$key['main']['encoded'] = encode_config(
+				generate_awg_full_conf($key_data, $srv, $i+1));
+			$key['relay']['encoded'] = $is_srv_relay ? '' : encode_config(
+				generate_awg_full_conf($key_data, $relay_srv, $i+1));
+
+			array_push($keys[$ukd->srv_name], $key);
 		}
-		$keys[$this_srv_name] = $this_srv_keys;
 	}
 	return $keys;
 }
 
-
 function generate_xray_keys($user_xray_key_data, $SERVER_DB, $relay_name) {
 	$keys = array();
 
-	for($i = 0; $i < count($user_xray_key_data); ++$i) {
-		$this_srv_key_data = $user_xray_key_data[$i]->key_data;
-		$this_srv_name = $user_xray_key_data[$i]->srv_name;
-		$this_srv_data = find_server($this_srv_name, $SERVER_DB);
-		$this_relay_srv_data = make_relay($this_srv_data, 
-			find_server($relay_name, $SERVER_DB));
+	foreach($user_xray_key_data as $ukd) {
+		$srv = find_server($ukd->srv_name, $SERVER_DB);
+		$relay_srv = make_relay($srv, find_server($relay_name, $SERVER_DB));
 
-		$this_srv_keys = array();
+		$is_srv_relay = ($srv->srv_name === $relay_name);
+		$keys[$ukd->srv_name] = array();
 
-		for($j = 0; $j < count($this_srv_key_data); ++$j) {
-			$this_srv_keys[$j]['main']['native'] = generate_xray_native_conf(
-				$this_srv_key_data[$j], $this_srv_data);
-			$this_srv_keys[$j]['relay']['native'] =
-				($this_srv_data->srv_name === $relay_name) ? '' :
-					generate_xray_native_conf(
-						$this_srv_key_data[$j], $this_relay_srv_data);
+		foreach($ukd->key_data as $i => $key_data) {
+			$key['main']['native'] =
+				generate_xray_native_conf($key_data, $srv);
+			$key['relay']['native'] = $is_srv_relay ? '' :
+				generate_xray_native_conf($key_data, $relay_srv);
 
-			$this_srv_keys[$j]['main']['encoded'] = encode_config(
-				generate_xray_full_conf(
-					$this_srv_key_data[$j], $this_srv_data, $j+1));
-			$this_srv_keys[$j]['relay']['encoded'] = 
-				($this_srv_data->srv_name === $relay_name) ? '' :
-					encode_config(generate_xray_full_conf(
-						$this_srv_key_data[$j], $this_relay_srv_data, $j+1));
+			$key['main']['encoded'] = encode_config(
+				generate_xray_full_conf($key_data, $srv, $i+1));
+			$key['relay']['encoded'] = $is_srv_relay ? '' : encode_config(
+				generate_xray_full_conf($key_data, $relay_srv, $i+1));
+
+			array_push($keys[$ukd->srv_name], $key);
 		}
-		$keys[$this_srv_name] = $this_srv_keys;
 	}
 	return $keys;
 }
@@ -102,26 +87,21 @@ function generate_frontend_data($user_data, $SERVER_DB, $relay_name) {
 	$xray_keys = generate_xray_keys(
 		$user_data->xray_key_data, $SERVER_DB, $relay_name);
 
-	for($i = 0; $i < count($SERVER_DB); ++$i) {
-		$user_has_awg_keys = array_key_exists(
-			$SERVER_DB[$i]->srv_name, $awg_keys);
-		$user_has_xray_keys = array_key_exists(
-			$SERVER_DB[$i]->srv_name, $xray_keys);
+	foreach($SERVER_DB as $srv) {
+		$srv_name = $srv->srv_name;
 
-		if (!$user_has_awg_keys && !$user_has_xray_keys) {
-			continue;
-		}
-
-		$srv_name = $SERVER_DB[$i]->srv_name;
+		$user_has_awg_keys = array_key_exists($srv_name, $awg_keys);
+		$user_has_xray_keys = array_key_exists($srv_name, $xray_keys);
+		if (!($user_has_awg_keys || $user_has_xray_keys)) continue;
 
 		$frontend_data['access_srv_data'][$srv_name]['name'] =
-			$SERVER_DB[$i]->display_name;
+			$srv->display_name;
 		$frontend_data['access_srv_data'][$srv_name]['location'] =
-			$SERVER_DB[$i]->location;
+			$srv->location;
 		$frontend_data['access_srv_data'][$srv_name]['description'] =
-			$SERVER_DB[$i]->description;
+			$srv->description;
 		$frontend_data['access_srv_data'][$srv_name]['display_order'] =
-			$SERVER_DB[$i]->display_order;
+			$srv->display_order;
 
 		$frontend_data['access_srv_data'][$srv_name]['awg_keys'] =
 			$user_has_awg_keys ? $awg_keys[$srv_name] : NULL;
@@ -131,7 +111,6 @@ function generate_frontend_data($user_data, $SERVER_DB, $relay_name) {
 
 	usort($frontend_data['access_srv_data'], 
 		function ($a, $b) {return $a['display_order'] - $b['display_order'];});
-
 
 	return $frontend_data;
 }
